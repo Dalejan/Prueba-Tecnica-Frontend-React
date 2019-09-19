4b5b0d57441d3268c8df841787dd72cf87af17d7
Dalejan <david1804997@gmail.com>
2019-09-19 04:46:09 -0500

    TODO: Ajustar searchbar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,14 +1,37 @@
 import React from "react";
 import styles from "./SearchBar.module.scss";
-
+import EmployeeListItem from "../EmployeeListItem/EmployeeListItem";
 /**
  * ## SearchBar
  * Componente para realizar búsquedas rápidas por cualquier criterio
  */
-const SearchBar = ({ search, onChangeSearch }) => {
+const SearchBar = props => {
+  const { search, onChangeSearch, results, className } = props;
   return (
-    <div className={styles.searchBar_container}>
-      <input type="text" value={search} onChange={onChangeSearch} />
+    <div className={[styles.searchBar_container, className].join(" ")}>
+      <label>Buscar</label>
+      <input
+        type="text"
+        value={search}
+        onChange={onChangeSearch}
+        className={styles.input}
+      />
+      <div className={styles.results_container}>
+        <div className={styles.list_head}>
+          <p>Nombre</p>
+          <p>Salario</p>
+          <p>Edad</p>
+        </div>
+        {results.map((e, key) => {
+          return (
+            <EmployeeListItem
+              employee={e}
+              key={key}
+              className={styles.list_body}
+            ></EmployeeListItem>
+          );
+        })}
+      </div>
     </div>
   );
 };
--- src/containers/EmployeeList/EmployeeList.js
+++ src/containers/EmployeeList/EmployeeList.js
@@ -7,6 +7,7 @@ import ErrorBoundary from "../../components/HOCS/ErrorBoundary";
 
 import styles from "./EmployeeList.module.scss";
 import empty_list from "../../assets/empty_list.svg";
+import { similarity } from "../../utils/search";
 
 /**
  * ## EmployeeList
@@ -15,64 +16,104 @@ import empty_list from "../../assets/empty_list.svg";
 const EmployeeList = () => {
   const [employees, setEmployees] = useState([]);
   const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
   const [loading, setLoading] = useState(true);
   const [errors, setErrors] = useState({});
 
+  /**
+   * Primera obtención del listado
+   */
   useEffect(() => {
     fetchEmployees();
   }, []);
 
+  /**
+   * ### fetchEmployees
+   * Petición para obtener el listado y verificar si se obtienen u ocurrieron errores
+   */
+
   const fetchEmployees = () => {
     setLoading(true);
 
-    getEmployees().then(
-      res => {
-        // setEmployees(res);
+    getEmployees()
+      .then(res => {
+        setEmployees(res);
         setLoading(false);
-      },
-      err => setErrors(err)
-    );
+      })
+      .catch(err => setErrors(err));
   };
+
+  /**
+   * ### handleChangeSearch
+   * @param {any} event
+   * Manejador del cambio de value en el input de la barra de búsqueda
+   */
   const handleChangeSearch = event => {
-    console.log("ev", event.target.value);
     setSearch(event.target.value);
+    if (event.target.value + "") {
+      let newR = employees.filter(em => {
+        return (
+          similarity(event.target.value, em.employee_name) > 0.4 ||
+          similarity(event.target.value, em.employee_salary) > 0.4 ||
+          similarity(event.target.value, em.employee_age) > 0.6
+        );
+      });
+      if (newR.length > 0) {
+        setResults(newR);
+      }
+    }
   };
 
   return (
     <ErrorBoundary>
-      <SearchBar
-        search={search}
-        onChangeSearch={ev => handleChangeSearch(ev)}
-      ></SearchBar>
-      {employees.length > 0 && !errors ? (
-        <>
-          <div className={styles.list_head}>
-            <p>Nombre</p>
-            <p>Salario</p>
-            <p>Edad</p>
-          </div>
-          {employees.map((e, key) => {
-            return (
-              <EmployeeListItem
-                employee={e}
-                key={key}
-                className={styles.list_body}
-              ></EmployeeListItem>
-            );
-          })}
-        </>
+      {loading ? (
+        <span className={styles.loading}>Cargando....</span>
       ) : (
-        <div className={styles.empty_list}>
-          <p>Parece que estamos teniendo problemas</p>{" "}
-          <img src={empty_list} alt="empty list"></img>
-          <button
-            onClick={fetchEmployees}
-            className={styles.btn_try}
-            disabled={loading}
-          >
-            Volver a Intentar
-          </button>
-        </div>
+        <>
+          {!errors.keys && !loading && employees.length ? (
+            <>
+              <SearchBar
+                search={search}
+                onChangeSearch={ev => handleChangeSearch(ev)}
+                className={styles.searchBar}
+                results={results}
+              ></SearchBar>
+              <div className={styles.list_head}>
+                <p>Nombre</p>
+                <p>Salario</p>
+                <p>Edad</p>
+              </div>
+              {employees.map((e, key) => {
+                return (
+                  <EmployeeListItem
+                    employee={e}
+                    key={key}
+                    className={styles.list_body}
+                  ></EmployeeListItem>
+                );
+              })}
+            </>
+          ) : (
+            <>
+              <div className={styles.empty_list}>
+                <p>
+                  {" "}
+                  {employees.length > 0
+                    ? "Parece que estamos teniendo problemas"
+                    : "No hemos encontrado nada"}
+                </p>{" "}
+                <img src={empty_list} alt="empty list"></img>
+                <button
+                  onClick={fetchEmployees}
+                  className={styles.btn_try}
+                  disabled={loading}
+                >
+                  Volver a Intentar
+                </button>
+              </div>
+            </>
+          )}
+        </>
       )}
     </ErrorBoundary>
   );
